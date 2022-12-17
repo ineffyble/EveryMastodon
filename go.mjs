ef56533d9d8985241521b19f6ad17408fb1e7392
Effy Elden <github@effy.is>
2022-12-17 17:27:25 +1100

    Create go.mjs

---
+++ go.mjs
@@ -0,0 +1,28 @@
+import {readDirSync, writeFileSync} from 'fs';
+
+const res = await fetch(`https://instances.social/api/1.0/instances/list?count=1800&include_down=false&include_closed=false&sort_by=active_users&sort_order=desc`, {
+    headers: {
+        Authorization: `Bearer ${process.env.INSTANCES_SOCIAL_API_KEY}`
+    }
+});
+
+const done = readDirSync('done');
+
+const data = await res.json();
+const tweets = [];
+for (let instance of data.instances) {
+    if (instance.info?.short_description) {
+        if (!done.includes(instance.name)) {
+              await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${IFTTT_API_KEY}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({value1: tweet})
+              });
+          console.log(`Tweeted for ${instance.name}`);
+          writeFileSync(`done/${instance.name}`, instance.name);
+          break;
+        }
+    }
+}
