bc1b113fb3fdb4b1b0fde1c37e992522e60eb10f
Effy Elden <github@effy.is>
2022-12-17 17:33:26 +1100

    Update go.mjs

--- go.mjs
+++ go.mjs
@@ -13,7 +13,7 @@ const tweets = [];
 for (let instance of data.instances) {
     if (instance.info?.short_description) {
         if (!done.includes(instance.name)) {
-              await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${IFTTT_API_KEY}`, {
+              await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${process.env.IFTTT_API_KEY}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
