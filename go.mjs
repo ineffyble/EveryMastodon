d91df985af0bcb2187af6a282c703e9604bcfacb
Effy Elden <github@effy.is>
2022-12-19 01:05:42 +1100

    Get the info from instance API

--- go.mjs
+++ go.mjs
@@ -1,4 +1,4 @@
-import {readdirSync, writeFileSync} from 'fs';
+import { readdirSync, writeFileSync } from 'fs';
 
 const res = await fetch(`https://instances.social/api/1.0/instances/list?count=1800&include_down=false&include_closed=false&sort_by=active_users&sort_order=desc`, {
     headers: {
@@ -11,19 +11,25 @@ const done = readdirSync('done');
 const data = await res.json();
 const tweets = [];
 for (let instance of data.instances) {
-    if (instance.info?.short_description) {
-        if (!done.includes(instance.name)) {
-              const tweet = `https://mastoredirect.netlify.app/${instance.name} ${instance.info.short_description.replace(instance.name, '').replace(instance.name, '')}`;
-              await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${process.env.IFTTT_API_KEY}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({value1: tweet})
-              });
-          console.log(`Tweeted for ${instance.name}`);
-          writeFileSync(`done/${instance.name}`, instance.name);
-          break;
+    if (!done.includes(instance.name)) {
+        try {
+            const req = await fetch(`https://${instance.name}/api/v1/instance`);
+            const info = req.json();
+            if (info.short_description) {
+                const tweet = `https://mastoredirect.netlify.app/${instance.name} ${info.short_description.replace(instance.name, '').replace(instance.name, '')}`;
+                await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${process.env.IFTTT_API_KEY}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ value1: tweet })
+                });
+                console.log(`Tweeted for ${instance.name}`);
+            }
+        } catch (err) {
+            console.log(`Error trying to tweet for ${instance.name}`);
         }
+        writeFileSync(`done/${instance.name}`, instance.name);
+        break;
     }
 }
