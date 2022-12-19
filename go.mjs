bb11953620ae6bb435453881e810211873c8a8e5
Effy Elden <github@effy.is>
2022-12-20 01:01:34 +1100

    get from joinmastodon too

--- go.mjs
+++ go.mjs
@@ -1,12 +1,45 @@
 import { readdirSync, writeFileSync } from 'fs';
 
+const done = readdirSync('done');
+
+const joinres = await fetch('https://api.joinmastodon.org/servers');
+const joindata = await res.json();
+
+let tweeted = false;
+
+for (let instance of joindata) {
+    if (!done.includes(instance.domain)) {
+                try {
+            const tweet = `https://mastoredirect.netlify.app/${instance.domain} ${info.description.replace(instance.domain, '').replace(instance.domain, '')}`;
+            await fetch(`https://maker.ifttt.com/trigger/make_tweet_happen/with/key/${process.env.IFTTT_API_KEY}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ value1: tweet })
+                });
+                console.log(`Tweeted for ${instance.domain}`);
+                tweeted = true;
+
+        } catch (err) {
+            console.log(`Error trying to tweet for ${instance.name}`);
+        }
+        writeFileSync(`done/${instance.name}`, instance.name);
+        break;
+    }
+}
+
+if (tweeted) {
+    process.env.exit();
+}
+
+
+
 const res = await fetch(`https://instances.social/api/1.0/instances/list?count=1800&include_down=false&include_closed=false&sort_by=active_users&sort_order=desc`, {
     headers: {
         Authorization: `Bearer ${process.env.INSTANCES_SOCIAL_API_KEY}`
     }
 });
-
-const done = readdirSync('done');
 
 const data = await res.json();
 const tweets = [];
